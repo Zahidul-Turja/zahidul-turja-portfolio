97d8567392d8c554425014bd514a46bde69606c1
Zahidul-Turja <zahidulturja@gmail.com>
2024-09-15 20:40:48 +0600

    Grag animation

--- app/_components/sections/TechStack.js
+++ app/_components/sections/TechStack.js
@@ -1,3 +1,5 @@
+"use client";
+
 import { FaReact, FaFigma } from "react-icons/fa";
 import { IoLogoNodejs } from "react-icons/io5";
 import { RiNextjsLine, RiTailwindCssFill } from "react-icons/ri";
@@ -13,6 +15,8 @@ import {
 } from "react-icons/si";
 import { TbBrandRedux } from "react-icons/tb";
 
+import { motion } from "framer-motion";
+
 function TechStack() {
   return (
     <div className="my-12 flex w-full flex-col items-center justify-between px-[10vw]">
@@ -23,48 +27,230 @@ function TechStack() {
       </h1> */}
       {/* <div className="w-[80%] grid grid-cols-8 items-center justify-between"> */}
       <div className="flex w-[75%] flex-wrap justify-center gap-8">
-        <div className="flex h-20 w-20 items-center justify-center rounded-full bg-slate-50 shadow-md shadow-primary-900/30">
+        <motion.div
+          className="flex h-20 w-20 cursor-grab items-center justify-center rounded-full bg-slate-50 shadow-md shadow-primary-900/30"
+          drag
+          dragConstraints={{
+            top: -12,
+            right: 12,
+            bottom: 12,
+            left: -12,
+          }}
+          dragTransition={{
+            bounceDamping: 10,
+            bounceStiffness: 600,
+          }}
+        >
           <RiNextjsLine className="text-5xl text-primary-900" />
-        </div>
-        <div className="flex h-20 w-20 items-center justify-center rounded-full bg-slate-50 shadow-md shadow-primary-900/30">
+        </motion.div>
+        <motion.div
+          className="flex h-20 w-20 cursor-grab items-center justify-center rounded-full bg-slate-50 shadow-md shadow-primary-900/30"
+          drag
+          dragConstraints={{
+            top: -12,
+            right: 12,
+            bottom: 12,
+            left: -12,
+          }}
+          dragTransition={{
+            bounceDamping: 10,
+            bounceStiffness: 600,
+          }}
+        >
           <FaReact className="text-5xl text-[#5ED3F3]" />
-        </div>
-        <div className="flex h-20 w-20 items-center justify-center rounded-full bg-slate-50 shadow-md shadow-primary-900/30">
+        </motion.div>
+        <motion.div
+          className="flex h-20 w-20 cursor-grab items-center justify-center rounded-full bg-slate-50 shadow-md shadow-primary-900/30"
+          drag
+          dragConstraints={{
+            top: -12,
+            right: 12,
+            bottom: 12,
+            left: -12,
+          }}
+          dragTransition={{
+            bounceDamping: 10,
+            bounceStiffness: 600,
+          }}
+        >
           <SiReactquery className="text-5xl text-[#FF4154]" />
-        </div>
-        <div className="flex h-20 w-20 items-center justify-center rounded-full bg-slate-50 shadow-md shadow-primary-900/30">
+        </motion.div>
+        <motion.div
+          className="flex h-20 w-20 cursor-grab items-center justify-center rounded-full bg-slate-50 shadow-md shadow-primary-900/30"
+          drag
+          dragConstraints={{
+            top: -12,
+            right: 12,
+            bottom: 12,
+            left: -12,
+          }}
+          dragTransition={{
+            bounceDamping: 10,
+            bounceStiffness: 600,
+          }}
+        >
           <TbBrandRedux className="text-5xl text-[#7248B6]" />
-        </div>
-        <div className="flex h-20 w-20 items-center justify-center rounded-full bg-slate-50 shadow-md shadow-primary-900/30">
+        </motion.div>
+        <motion.div
+          className="flex h-20 w-20 cursor-grab items-center justify-center rounded-full bg-slate-50 shadow-md shadow-primary-900/30"
+          drag
+          dragConstraints={{
+            top: -12,
+            right: 12,
+            bottom: 12,
+            left: -12,
+          }}
+          dragTransition={{
+            bounceDamping: 10,
+            bounceStiffness: 600,
+          }}
+        >
           <SiExpress className="text-5xl text-primary-900" />
-        </div>
-        <div className="flex h-20 w-20 items-center justify-center rounded-full bg-slate-50 shadow-md shadow-primary-900/30">
+        </motion.div>
+        <motion.div
+          className="flex h-20 w-20 cursor-grab items-center justify-center rounded-full bg-slate-50 shadow-md shadow-primary-900/30"
+          drag
+          dragConstraints={{
+            top: -12,
+            right: 12,
+            bottom: 12,
+            left: -12,
+          }}
+          dragTransition={{
+            bounceDamping: 10,
+            bounceStiffness: 600,
+          }}
+        >
           <IoLogoNodejs className="text-5xl text-[#428A3C]" />
-        </div>
-        <div className="flex h-20 w-20 items-center justify-center rounded-full bg-slate-50 shadow-md shadow-primary-900/30">
+        </motion.div>
+        <motion.div
+          className="flex h-20 w-20 cursor-grab items-center justify-center rounded-full bg-slate-50 shadow-md shadow-primary-900/30"
+          drag
+          dragConstraints={{
+            top: -12,
+            right: 12,
+            bottom: 12,
+            left: -12,
+          }}
+          dragTransition={{
+            bounceDamping: 10,
+            bounceStiffness: 600,
+          }}
+        >
           <SiMongodb className="text-5xl text-[#428A3C]" />
-        </div>
-        <div className="flex h-20 w-20 items-center justify-center rounded-full bg-slate-50 shadow-md shadow-primary-900/30">
+        </motion.div>
+        <motion.div
+          className="flex h-20 w-20 cursor-grab items-center justify-center rounded-full bg-slate-50 shadow-md shadow-primary-900/30"
+          drag
+          dragConstraints={{
+            top: -12,
+            right: 12,
+            bottom: 12,
+            left: -12,
+          }}
+          dragTransition={{
+            bounceDamping: 10,
+            bounceStiffness: 600,
+          }}
+        >
           <RiTailwindCssFill className="text-5xl text-[#13B9B3]" />
-        </div>
-        <div className="flex h-20 w-20 items-center justify-center rounded-full bg-slate-50 shadow-md shadow-primary-900/30">
+        </motion.div>
+        <motion.div
+          className="flex h-20 w-20 cursor-grab items-center justify-center rounded-full bg-slate-50 shadow-md shadow-primary-900/30"
+          drag
+          dragConstraints={{
+            top: -12,
+            right: 12,
+            bottom: 12,
+            left: -12,
+          }}
+          dragTransition={{
+            bounceDamping: 10,
+            bounceStiffness: 600,
+          }}
+        >
           <SiChakraui className="text-5xl text-[#4FD1C5]" />
-        </div>
-        <div className="flex h-20 w-20 items-center justify-center rounded-full bg-slate-50 shadow-md shadow-primary-900/30">
+        </motion.div>
+        <motion.div
+          className="flex h-20 w-20 cursor-grab items-center justify-center rounded-full bg-slate-50 shadow-md shadow-primary-900/30"
+          drag
+          dragConstraints={{
+            top: -12,
+            right: 12,
+            bottom: 12,
+            left: -12,
+          }}
+          dragTransition={{
+            bounceDamping: 10,
+            bounceStiffness: 600,
+          }}
+        >
           <SiSupabase className="text-5xl text-[#2D7555]" />
-        </div>
-        <div className="flex h-20 w-20 items-center justify-center rounded-full bg-slate-50 shadow-md shadow-primary-900/30">
+        </motion.div>
+        <motion.div
+          className="flex h-20 w-20 cursor-grab items-center justify-center rounded-full bg-slate-50 shadow-md shadow-primary-900/30"
+          drag
+          dragConstraints={{
+            top: -12,
+            right: 12,
+            bottom: 12,
+            left: -12,
+          }}
+          dragTransition={{
+            bounceDamping: 10,
+            bounceStiffness: 600,
+          }}
+        >
           <SiMysql className="text-5xl text-[#00718B]" />
-        </div>
-        <div className="flex h-20 w-20 items-center justify-center rounded-full bg-slate-50 shadow-md shadow-primary-900/30">
+        </motion.div>
+        <motion.div
+          className="flex h-20 w-20 cursor-grab items-center justify-center rounded-full bg-slate-50 shadow-md shadow-primary-900/30"
+          drag
+          dragConstraints={{
+            top: -12,
+            right: 12,
+            bottom: 12,
+            left: -12,
+          }}
+          dragTransition={{
+            bounceDamping: 10,
+            bounceStiffness: 600,
+          }}
+        >
           <FaFigma className="text-5xl text-secondary-800" />
-        </div>
-        <div className="flex h-20 w-20 items-center justify-center rounded-full bg-slate-50 shadow-md shadow-primary-900/30">
+        </motion.div>
+        <motion.div
+          className="flex h-20 w-20 cursor-grab items-center justify-center rounded-full bg-slate-50 shadow-md shadow-primary-900/30"
+          drag
+          dragConstraints={{
+            top: -12,
+            right: 12,
+            bottom: 12,
+            left: -12,
+          }}
+          dragTransition={{
+            bounceDamping: 10,
+            bounceStiffness: 600,
+          }}
+        >
           <SiDjango className="text-5xl text-[#092D1F]" />
-        </div>
-        <div className="flex h-20 w-20 items-center justify-center rounded-full bg-slate-50 shadow-md shadow-primary-900/30">
+        </motion.div>
+        <motion.div
+          className="flex h-20 w-20 cursor-grab items-center justify-center rounded-full bg-slate-50 shadow-md shadow-primary-900/30"
+          drag
+          dragConstraints={{
+            top: -12,
+            right: 12,
+            bottom: 12,
+            left: -12,
+          }}
+          dragTransition={{
+            bounceDamping: 10,
+            bounceStiffness: 600,
+          }}
+        >
           <SiTensorflow className="text-5xl text-[#F77500]" />
-        </div>
+        </motion.div>
       </div>
     </div>
   );
